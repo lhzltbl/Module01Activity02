00a4f5efd6a868b7be1ce9531324c8cce45e71c4
lhzltbl <lhizel.0415@gmail.com>
2024-08-21 12:13:03 +0800

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Module01Activty02
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace Module01Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Firstname { get; set; }
+        public Color ColorRed { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Firstname = "Hello, MAUI";
+            ColorRed = Colors.Red;
+
+            this.BindingContext = this;
+        }
+
+        private void OnChangedLabelName(object sender, EventArgs e)
+        {
+            this.Resources["StaticUsername"] = "Peter Butter";
+        }
+
+        private void OnChangedLabelNameDynamic(object sender, EventArgs e)
+        {
+            this.Resources["DynamicUsername"] = "Mr. John Doe";
+        }
+
+        private void OnChangedBackgroundColorGray(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightGray;
+        }
+
+        private void OnChangedBackgroundColorGreen(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Green;
+        }
+
+        private void OnChangedBackgroundColorLightBlue(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightBlue;
+        }
+
+        private void OnChangedBoxViewColor(object sender, EventArgs e)
+        {
+            this.Resources["DynamicBoxColor"] = Colors.Red;
+        }
+    }
+
+}
